4f1519c7d2862257d99d9ab08a22103ec03b5f5a
Goncear Valera <valeragoncear@gmail.com>
2024-05-03 16:22:20 +0300

    slider added

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,18 @@
 import '../scss/style.scss';
 
+// const swiper = new Swiper(".mySwiper", {
+//     slidesPerView: "auto",
+//     centeredSlides: true,
+//     spaceBetween: 30,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
+
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
