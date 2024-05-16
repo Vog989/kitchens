c2a59257f1c52a49eef50920a83b379a7b6b3d8b
Goncear Valera <valeragoncear@gmail.com>
2024-05-16 16:46:04 +0300

    faq

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,22 @@ const swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const questions = document.querySelectorAll('.faq__question');
+
+    questions.forEach(question => {
+        question.addEventListener('click', function() {
+            const answer = this.nextElementSibling;
+            const isOpen = this.classList.contains('active');
+
+            if (isOpen) {
+                answer.style.maxHeight = 0;
+            } else {
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            }
+
+            this.classList.toggle('active');
+        });
+    });
+});
